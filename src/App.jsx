7a5ede638f226586caab07cba023c7b3f23ba8bb
asg1998f <asg1998f@gmail.com>
2025-01-11 16:37:30 +0100

    auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import { useState } from 'react'
 import './App.css'
 import Register from './components/Register/Register'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from './components/Login/Login'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-    <Register/>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path="/register" element={<Register/>} />
+      <Route path="/login" element={<Login/>} />
+    </Routes>
+    <Footer/>
+    </BrowserRouter>
     </>
   )
 }
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from 'react'
+import { register } from '../../features/auth/authSlice'
+import { useDispatch } from 'react-redux'
+import { notification } from 'antd'
 const Register = () => {
     const [formData, setFormData] = useState({
         name:'',
         email:'',
-        password:''
+        password:'',
+        password2:'',
+        birthday:''
     })
-    const {name,email,password} = formData
+    const {name,email,password,birthday,password2} = formData
+    const dispatch = useDispatch()
     const onChange = (e)=>{
         setFormData({
             ...formData,
@@ -14,13 +20,23 @@ const Register = () => {
     }
     const onSubmit = (e) => {
         e.preventDefault()
-        console.log('formData',formData)
+        if (password !== password2) {
+            return notification.error({
+              message: "Error",
+              description: "Passwords do not match",
+            });
+          } else {
+              return dispatch(register(formData));
+          }
+      
     }
   return (
     <form onSubmit={onSubmit}>
         <input type="text" name="name" value={name} placeholder="Name" onChange={onChange} />
         <input type="email" name="email" value={email} placeholder="Email" onChange={onChange}/>
         <input type="password" name="password" value={password} placeholder="Password" onChange={onChange}/>
+        <input type="password" name="password2" value={password2} placeholder="Password 2" onChange={onChange}/>
+        <input type="date" name="birthday" value={birthday} placeholder="Birthday" onChange={onChange}/>
         <button type="submit">Register</button>
     </form>
   )
